545de8705e327b9d476ed3b4ef164c292ac84a1a
Kamal23052004 <itx.kamal.official@gmail.com>
2024-05-11 11:45:46 +0530

    1- posts routes and controllers added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -50,12 +50,19 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ""
     },
+    likedPosts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+            default: [],
+        },
+    ],
 }, 
 {timestamps: true}
 );
 
 
 
-const User = mongoose.model("Users", userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
