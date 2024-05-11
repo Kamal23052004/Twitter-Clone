75850c79850930a8d27fa7d67d276a955182700f
Kamal23052004 <itx.kamal.official@gmail.com>
2024-05-11 12:41:02 +0530

    1- notification routes and controllers added

--- backend/models/notification.model.js
+++ backend/models/notification.model.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 const notificationSchema = new mongoose.Schema({
     from:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Users',
+        ref: 'User',
         required: true
     },
     to: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Users',
+        ref: 'User',
         required: true
     },
     type: {
