8230c62e2e94c728245b85452120dac6e4bf393c
Kamal23052004 <itx.kamal.official@gmail.com>
2024-05-12 17:32:25 +0530

    3- suggested users fetched and fixed some bugs

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -63,30 +63,31 @@ export const followUnfollowUser = async (req, res) => {
 };
 
 export const getSuggestedUsers = async (req, res) => {
-    try {
-       const userId = req.user._id;
+	try {
+		const userId = req.user._id;
 
-       const userFollowedByMe = await User.findById(userId).select("following");
+		const usersFollowedByMe = await User.findById(userId).select("following");
 
-       const users = await User.aggregate([
-        {
-            $match: {
-                _id: { $ne: userId},
-            },
-        },
-        { $sample: { size: 10} },
-       ]);
+		const users = await User.aggregate([
+			{
+				$match: {
+					_id: { $ne: userId },
+				},
+			},
+			{ $sample: { size: 10 } },
+		]);
 
-       //123456,
-       const filteredUsers = users.filter((user) => !userFollowedByMe.following.includes(user._id));
-       const suggestedUsers = filteredUsers.slice(0, 4);
+		// 1,2,3,4,5,6,
+		const filteredUsers = users.filter((user) => !usersFollowedByMe.following.includes(user._id));
+		const suggestedUsers = filteredUsers.slice(0, 4);
 
-       suggestedUsers.forEach((user) => (user.password = null));
-        
-    } catch (error) {
-        console.log("Error in getSuggestedUsers: ", error.message);
-        res.status(500).json({ error: error.message });
-    }
+		suggestedUsers.forEach((user) => (user.password = null));
+
+		res.status(200).json(suggestedUsers);
+	} catch (error) {
+		console.log("Error in getSuggestedUsers: ", error.message);
+		res.status(500).json({ error: error.message });
+	}
 };
 
 export const updateUser = async (req, res) => {
